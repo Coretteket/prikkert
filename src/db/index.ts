50c169bec30fa926d88602f26fa3ae3eae370d67
qcoret <qcoret@gmail.com>
2025-01-26 15:19:07 +0100

    node-postgres -> postgres.js

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,10 @@
-import { drizzle } from 'drizzle-orm/node-postgres'
+import postgres from 'postgres'
+import { drizzle } from 'drizzle-orm/postgres-js'
 import * as schema from './schema'
 import { NODE_ENV } from '$env/static/private'
 import { env } from '$env/dynamic/private'
 
-export const db = drizzle(env.DATABASE_URL, { schema, logger: NODE_ENV === 'development' })
+const client = postgres(env.DATABASE_URL)
+export const db = drizzle({ client, schema, logger: NODE_ENV === 'development' })
 
 export { schema }
