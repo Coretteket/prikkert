547f17a3e10fd4a72dfb9a1c9af4490edcd5453e
qcoret <qcoret@gmail.com>
2025-02-02 15:46:14 +0100

    okay this should actually work this time

--- src/routes/afspraak/maken/time-input.svelte
+++ src/routes/afspraak/maken/time-input.svelte
@@ -6,7 +6,7 @@
 
 <input
 	type="time"
-	class="w-16 rounded border px-1 py-0.5 text-center tabular-nums"
+	class="w-16 rounded border px-1 py-0.5 text-center tabular-nums appearance-none"
 	bind:value={
 		() => time?.toString().slice(0, 5) ?? '00:00',
 		(value) => (time = PlainTime.from(value ?? '00:00'))
