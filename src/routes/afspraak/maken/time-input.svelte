f2db08aae87455d9e0f742a1874c47e9567ef3e5
qcoret <qcoret@gmail.com>
2025-02-02 15:39:10 +0100

    i hate default css!

--- src/routes/afspraak/maken/time-input.svelte
+++ src/routes/afspraak/maken/time-input.svelte
@@ -6,7 +6,7 @@
 
 <input
 	type="time"
-	class="w-16 rounded border px-1 py-0.5 text-center tabular-nums [&::-webkit-calendar-picker-indicator]:hidden"
+	class="w-16 rounded border px-1 py-0.5 text-center tabular-nums"
 	bind:value={
 		() => time?.toString().slice(0, 5) ?? '00:00',
 		(value) => (time = PlainTime.from(value ?? '00:00'))
