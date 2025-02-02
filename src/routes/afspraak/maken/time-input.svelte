7a1dced86c39ed5c90fca5b3d758cb8c1da4136d
qcoret <qcoret@gmail.com>
2025-02-02 15:50:25 +0100

    !!!

--- src/routes/afspraak/maken/time-input.svelte
+++ src/routes/afspraak/maken/time-input.svelte
@@ -6,9 +6,16 @@
 
 <input
 	type="time"
-	class="w-16 rounded border px-1 py-0.5 text-center tabular-nums appearance-none"
+	class="w-16 appearance-none rounded border px-1 py-0.5 text-center tabular-nums"
 	bind:value={
 		() => time?.toString().slice(0, 5) ?? '00:00',
 		(value) => (time = PlainTime.from(value ?? '00:00'))
 	}
 />
+
+<style>
+	input[type='time']::-webkit-date-and-time-value {
+		margin: 0;
+		width: 100%;
+	}
+</style>
