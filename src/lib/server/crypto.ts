db6008b174d0c69ba62efc9e51cc844932df642b
qcoret <qcoret@gmail.com>
2025-02-09 20:53:29 +0100

    remove unused fn

--- src/lib/server/crypto.ts
+++ src/lib/server/crypto.ts
@@ -10,28 +10,15 @@ export const generateNanoid = customAlphabet(
 export async function encode(
 	algorithm: AlgorithmIdentifier,
 	bufferEncoding: BufferEncoding,
-	input: string,
+	input?: string | null,
 ) {
+	if (!input) return ''
 	const bytes = new TextEncoder().encode(input)
 	const hash = await crypto.subtle.digest(algorithm, bytes)
 	return Buffer.from(hash).toString(bufferEncoding)
 }
 
 /** Encodes a string using SHA-256. */
-export function encodeSHA256(input: string) {
+export function encodeSHA256(input?: string | null) {
 	return encode('SHA-256', 'base64', input)
-}
-
-/** Checks if a given password has been compromised by querying the "Have I Been Pwned" API. */
-export async function checkPasswordPwned(password: string) {
-	const hash = await encode('SHA-1', 'hex', password)
-	const hashPrefix = hash.slice(0, 5)
-	const response = await fetch(`https://api.pwnedpasswords.com/range/${hashPrefix}`)
-	if (!response.ok) return true
-	const data = await response.text()
-	const items = data.split('\n')
-	return items.some((item) => {
-		const hashSuffix = item.slice(0, 35).toLowerCase()
-		return hash === hashPrefix + hashSuffix
-	})
 }
