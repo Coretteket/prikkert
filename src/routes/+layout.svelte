ec4cdbe4286f6f1efb3b1277ea1d6f5bc5db377c
qcoret <qcoret@gmail.com>
2025-02-15 22:46:07 +0100

    distinguishing features on landing page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,8 +6,28 @@
 	import icon from '@/lib/assets/icon.svg'
 	import pattern from '@/lib/assets/pattern.svg'
 	import Button from '@/lib/components/button.svelte'
+	import { IconMoon, IconSun } from '@tabler/icons-svelte'
+	import { browser } from '$app/environment'
 
 	let { data, children } = $props()
+
+	function parseCookie(key: string) {
+		if (!browser) return 'light'
+		const cookie = document.cookie.split('; ').find((row) => row.startsWith(`${key}=`))
+		return cookie ? cookie.split('=')[1] : 'light'
+	}
+
+	let theme = $state(parseCookie('theme') === 'light' ? 'light' : 'dark')
+
+	$effect(() => {
+		if (!browser) return
+		document.documentElement.classList.add('no-transition')
+		const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+		document.cookie = `theme=${theme === systemTheme ? 'system' : theme}; path=/; max-age=31536000`
+		document.documentElement.dataset.theme = theme
+		document.documentElement.offsetHeight
+		document.documentElement.classList.remove('no-transition')
+	})
 </script>
 
 <svelte:head>
@@ -21,22 +41,31 @@
 ></div>
 
 <div
-	class="bg-white p-2 sm:mx-auto sm:my-8 sm:max-w-[min(42rem,_calc(100vw_-_2rem))] sm:rounded-lg sm:border sm:p-4 dark:bg-zinc-900 dark:sm:border-zinc-800"
+	class="bg-white p-2 sm:mx-auto sm:my-8 sm:max-w-[min(48rem,_calc(100vw_-_3rem))] sm:rounded-lg sm:border sm:p-4 dark:bg-zinc-900 dark:sm:border-zinc-800"
 >
 	<nav class="flex items-center justify-between px-4 py-2">
 		<a
 			href="/"
-			class="font-display flex items-center gap-3 text-xl text-zinc-800 dark:text-zinc-300"
+			class="font-display flex items-center gap-3 text-2xl text-zinc-800 dark:text-zinc-300"
 		>
 			<img src={icon} width={32} height={32} alt="" />
 			Prikkert
 		</a>
 
-		{#if data.hasSession}
-			<Button as="link" href="/afspraken">Jouw afspraken</Button>
-		{:else}
-			<Button as="link" href="/afspraak/maken">Afspraak maken</Button>
-		{/if}
+		<div class="flex gap-4">
+			<button onclick={() => (theme = theme === 'dark' ? 'light' : 'dark')} class="opacity-75">
+				{#if theme === 'dark'}
+					<IconSun />
+				{:else}
+					<IconMoon />
+				{/if}
+			</button>
+			{#if data.hasSession}
+				<Button as="link" href="/afspraken">Jouw afspraken</Button>
+			{:else}
+				<Button as="link" href="/afspraak/maken">Afspraak maken</Button>
+			{/if}
+		</div>
 	</nav>
 	<main class="p-4 pt-8">
 		{@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,108 @@
 <script lang="ts">
-	import Button from '@/lib/components/button.svelte'
+	import { IconAd, IconCode, IconMoneybag, IconShield } from '@tabler/icons-svelte'
 </script>
 
-<header class="my-8 space-y-4">
+<header class="my-10 space-y-4">
 	<h2
-		class="font-display mb-8 text-center text-3xl font-semibold sm:text-[2.5rem] sm:leading-tight"
+		class="font-display mb-8 text-center text-4xl font-semibold text-zinc-900 sm:text-[2.75rem] sm:leading-tight dark:text-zinc-100"
 	>
 		<p>Samen plannen,</p>
 		<p>zo geprikt</p>
 	</h2>
 	<h3
-		class="mx-auto mb-8 max-w-60 text-center text-lg font-[350] text-balance text-zinc-700 sm:max-w-100 dark:text-zinc-400"
+		class="mx-auto mb-8 max-w-80 text-center text-xl font-[350] text-zinc-700 sm:max-w-100 sm:text-balance dark:text-zinc-300"
 	>
 		Vind
-		<strong class="font-medium text-pink-700 dark:text-pink-500">gratis</strong>
+		<strong class="font-semibold text-pink-700 dark:text-zinc-200">gratis</strong>
 		én
-		<strong class="font-medium text-pink-700 dark:text-pink-500">reclamevrij</strong>
+		<strong class="font-semibold text-pink-700 dark:text-zinc-200">reclamevrij</strong>
 		het beste moment voor elke groepsafspraak.
 	</h3>
-	<Button as="link" href="/afspraak/maken" size="lg" color="primary" class="mx-auto">
+	<a
+		href="/afspraak/maken"
+		class="mx-auto block w-fit rounded-lg bg-pink-700 px-4 py-2 text-lg font-semibold text-white shadow transition hover:bg-pink-800 dark:bg-pink-800 dark:text-zinc-100 dark:hover:bg-pink-900"
+	>
 		Afspraak maken
-	</Button>
+	</a>
+	<!-- <Button as="link" href="/afspraak/maken" size="lg" color="primary" class="mx-auto">
+		Afspraak maken
+	</Button> -->
 </header>
+
+<section class="mt-24">
+	<h3 class="font-display mb-4 text-center text-xl font-semibold">Waarom Prikkert?</h3>
+	<p class="class mx-auto mb-8 max-w-120 text-center text-balance text-zinc-600 dark:text-zinc-300">
+		Datumprikken kan ook zonder verdienmodel. Daarom doet Prikkert vier simpele beloftes.
+	</p>
+	<div class="grid gap-4 sm:grid-cols-2">
+		<div
+			class="rounded-lg border from-zinc-900 to-pink-700/5 px-6 py-5 dark:border-zinc-800 dark:bg-gradient-to-tl"
+		>
+			<div class="mb-4 flex items-center gap-4">
+				<div
+					class="aspect-square w-fit rounded-lg bg-pink-500/20 p-2.5 text-pink-700 dark:text-pink-100"
+				>
+					<IconMoneybag />
+				</div>
+				<h4 class="font-display text-xl font-medium text-zinc-800 dark:text-zinc-200">
+					Altijd gratis
+				</h4>
+			</div>
+			<p class="font-[350] text-zinc-600 sm:text-balance dark:text-pink-50/70">
+				Geen verborgen kosten, geen verrassingen. Alle functies van Prikkert blijven altijd gratis.
+			</p>
+		</div>
+		<div
+			class="rounded-lg border from-zinc-900 to-pink-700/5 px-6 py-5 dark:border-zinc-800 dark:bg-gradient-to-tl"
+		>
+			<div class="mb-4 flex items-center gap-4">
+				<div
+					class="aspect-square w-fit rounded-lg bg-pink-500/20 p-2.5 text-pink-700 dark:text-pink-100"
+				>
+					<IconAd />
+				</div>
+				<h4 class="font-display text-xl font-medium text-zinc-800 dark:text-zinc-200">
+					Nooit reclame
+				</h4>
+			</div>
+			<p class="font-[350] text-zinc-600 sm:text-balance dark:text-pink-50/70">
+				Waarom zou je langs eindeloze advertenties moeten scrollen? Prikkert houdt het reclamevrij.
+			</p>
+		</div>
+		<div
+			class="rounded-lg border from-zinc-900 to-pink-700/5 px-6 py-5 dark:border-zinc-800 dark:bg-gradient-to-tl"
+		>
+			<div class="mb-4 flex items-center gap-4">
+				<div
+					class="aspect-square w-fit rounded-lg bg-pink-500/20 p-2.5 text-pink-700 dark:text-pink-100"
+				>
+					<IconShield />
+				</div>
+				<h4 class="font-display text-xl font-medium text-zinc-800 dark:text-zinc-200">
+					Privacy voorop
+				</h4>
+			</div>
+			<p class="font-[350] text-zinc-600 sm:text-balance dark:text-pink-50/70">
+				Jouw gegevens zijn van jou. Prikkert deelt niets met anderen en bewaart alles veilig in de
+				EU.
+			</p>
+		</div>
+		<div
+			class="rounded-lg border from-zinc-900 to-pink-700/5 px-6 py-5 dark:border-zinc-800 dark:bg-gradient-to-tl"
+		>
+			<div class="mb-4 flex items-center gap-4">
+				<div
+					class="aspect-square w-fit rounded-lg bg-pink-500/20 p-2.5 text-pink-700 dark:text-pink-100"
+				>
+					<IconCode />
+				</div>
+				<h4 class="font-display text-xl font-medium text-zinc-800 dark:text-zinc-200">
+					Open source
+				</h4>
+			</div>
+			<p class="font-[350] text-zinc-600 sm:text-balance dark:text-pink-50/70">
+				Iedereen kan de broncode lezen en verbeteren, want Prikkert heeft niets te verbergen.
+			</p>
+		</div>
+	</div>
+</section>
