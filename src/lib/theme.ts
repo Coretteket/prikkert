2d065a46c1bd7e3a142ca94f03eae546d0f03479
qcoret <qcoret@gmail.com>
2025-02-15 23:55:16 +0100

    this should work

--- src/lib/theme.ts
+++ src/lib/theme.ts
@@ -1,3 +1,5 @@
+import { browser } from '$app/environment'
+
 export type Theme = 'light' | 'dark' | 'system'
 
 export function setTheme(query: { matches: boolean }) {
@@ -14,4 +16,11 @@ export function toggleTheme() {
 	const theme = setTheme({ matches: document.documentElement.dataset.theme === 'light' })
 	const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
 	document.cookie = `theme=${theme === systemTheme ? 'system' : theme}; path=/; max-age=31536000`
+}
+
+// On load, immediately set the theme based on the system preference.
+if (browser && document.documentElement.getAttribute('data-theme') === 'system') {
+	const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
+	mediaQuery.addEventListener('change', setTheme)
+	setTheme(mediaQuery)
 }
