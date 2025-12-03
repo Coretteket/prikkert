06925d1a651f4083a840a6128625ca22e400dd8f
qcoret <qcoret@gmail.com>
2025-12-03 23:09:51 +0100

    nit: refactor basic auth to different file

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,28 +1,13 @@
 import type { Handle } from '@sveltejs/kit'
 
-import { env } from '$env/dynamic/private'
-
-import { getRequestEvent } from '$app/server'
-
+import { unauthenticated, validateBasicAuth } from '@/server/basic-auth'
 import { parseSessionCookies } from '@/server/session'
 import * as v from '@/server/validation'
-
-function checkBasicAuth() {
-	if (env.BASIC_AUTH !== "1") return true
-	const authorization = getRequestEvent().request.headers.get('authorization')
-	if (!authorization || !authorization.startsWith('Basic ')) return false
-	const credentials = Buffer.from(authorization.slice(6), 'base64').toString('utf8')
-	return credentials === `${env.BASIC_AUTH_USERNAME}:${env.BASIC_AUTH_PASSWORD}`
-}
 
 const ThemeSchema = v.fallback(v.picklist(['light', 'dark', 'system']), 'system')
 
 export const handle: Handle = async ({ event, resolve }) => {
-	if (!checkBasicAuth())
-		return new Response(env.BASIC_AUTH_MESSAGE, {
-			headers: { 'WWW-Authenticate': 'Basic realm="prikkert"' },
-			status: 401,
-		})
+	if (!validateBasicAuth()) return unauthenticated()
 
 	event.locals.session = parseSessionCookies()
 
---
+++ src/lib/server/basic-auth.ts
@@ -0,0 +1,17 @@
+import { env } from '$env/dynamic/private'
+
+import { getRequestEvent } from '$app/server'
+
+export function validateBasicAuth() {
+	if (env.BASIC_AUTH !== '1') return true
+	const authorization = getRequestEvent().request.headers.get('authorization')
+	if (!authorization || !authorization.startsWith('Basic ')) return false
+	const credentials = Buffer.from(authorization.slice(6), 'base64').toString('utf8')
+	return credentials === `${env.BASIC_AUTH_USERNAME}:${env.BASIC_AUTH_PASSWORD}`
+}
+
+export const unauthenticated = () =>
+	new Response(env.BASIC_AUTH_MESSAGE, {
+		headers: { 'WWW-Authenticate': 'Basic realm="prikkert"' },
+		status: 401,
+	})
