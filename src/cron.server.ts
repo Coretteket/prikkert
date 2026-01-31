be8e4f849935fa37cb7f03f0a7ba4df2b775a38d
qcoret <qcoret@gmail.com>
2026-01-31 17:17:33 +0100

    refactor: cron code

---
+++ src/cron.server.ts
@@ -0,0 +1,25 @@
+import { lt } from 'drizzle-orm'
+import { CronJob } from 'cron'
+
+import { Temporal } from '@/shared/temporal'
+import { db, schema } from '@/server/db'
+
+export const cron = CronJob.from({
+	cronTime: '0 5 * * *',
+	timeZone: 'Europe/Amsterdam',
+	onTick: async () => {
+		try {
+			const now = Temporal.Now.instant().toString()
+
+			const result = await db
+				.delete(schema.events)
+				.where(lt(schema.events.expiresAt, now))
+				.returning({ id: schema.events.id })
+
+			if (result.length > 0) console.log(`[CRON] Deleted ${result.length} expired events.`)
+			else console.log('[CRON] No expired events found.')
+		} catch (error) {
+			console.error('[CRON] Event cleanup failed:', error)
+		}
+	},
+})
--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,39 +1,17 @@
 import { redirect, type Handle } from '@sveltejs/kit'
-import { lt } from 'drizzle-orm'
-import { CronJob } from 'cron'
 
 import { building, dev } from '$app/environment'
 
 import { unauthenticated, validateBasicAuth } from '@/server/basic-auth'
 import { parseSessionCookies } from '@/server/session/cookies'
 import { ID_LENGTH } from '@/server/db/schema'
-import { Temporal } from '@/shared/temporal'
 import * as v from '@/server/validation'
-import { db, schema } from '@/server/db'
+
+import { cron } from './cron.server'
 
 export const init = async () => {
 	if (dev || building) return
-
-	CronJob.from({
-		cronTime: '0 5 * * *',
-		timeZone: 'Europe/Amsterdam',
-		start: true,
-		onTick: async () => {
-			try {
-				const now = Temporal.Now.instant().toString()
-
-				const result = await db
-					.delete(schema.events)
-					.where(lt(schema.events.expiresAt, now))
-					.returning({ id: schema.events.id })
-
-				if (result.length > 0) console.log(`[CRON] Deleted ${result.length} expired events.`)
-				else console.log('[CRON] No expired events found.')
-			} catch (error) {
-				console.error('[CRON] Event cleanup failed:', error)
-			}
-		},
-	})
+	cron.start()
 }
 
 const ThemeSchema = v.fallback(v.picklist(['light', 'dark', 'system']), 'system')
