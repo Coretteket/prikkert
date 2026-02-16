4762de7d3cd50aba552d65dc7f8cc57bc018d706
qcoret <qcoret@gmail.com>
2026-02-16 22:23:05 +0100

    fix: empty state

--- src/routes/afspraken/+page.svelte
+++ src/routes/afspraken/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { formatDateTimeRange } from '@/shared/time-format'
-	import { capitalizeFirst } from '@/shared/utils'
+	import Button from '@/components/button.svelte'
 
 	import { getEvents } from './data.remote'
 
@@ -19,7 +19,7 @@
 			class="block overflow-hidden rounded-lg border px-6 py-5 hover:bg-neutral-50 motion-safe:transition-colors dark:hover:bg-neutral-800/50"
 		>
 			<h2 class="mb-2 text-lg font-semibold text-neutral-700 dark:text-neutral-300">
-				{capitalizeFirst(event.title)}
+				{event.title}
 			</h2>
 
 			<p class="text-neutral-600 dark:text-neutral-400">
@@ -28,5 +28,21 @@
 				{numberResponses(event.numberOfResponses)}
 			</p>
 		</a>
+	{:else}
+		<div
+			class="rounded-lg border border-neutral-300 px-6 py-9 text-center text-neutral-600 dark:border-neutral-700 dark:text-neutral-400"
+		>
+			<p class="text-lg">Je hebt nog niet gereageerd of een afspraak aangemaakt.</p>
+		</div>
 	{/each}
 </div>
+
+<Button
+	variant="primary"
+	as="link"
+	href="/afspraak/aanmaken"
+	size="md"
+	class="mt-10 ml-auto"
+>
+	Afspraak aanmaken
+</Button>
