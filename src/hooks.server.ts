eca87d649266a44fff464c2244342b2abdb8c1da
qcoret <qcoret@gmail.com>
2026-02-19 16:59:22 +0100

    feat: remove basic auth guard

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,7 +2,6 @@ import { redirect, type Handle } from '@sveltejs/kit'
 
 import { building, dev } from '$app/environment'
 
-import { unauthenticated, validateBasicAuth } from '@/server/basic-auth'
 import { parseSessionCookies } from '@/server/session/cookies'
 import { ID_LENGTH } from '@/server/db/schema'
 import { parseTheme } from '@/server/theme'
@@ -12,8 +11,6 @@ import { cron } from './cron.server'
 export const init = () => !dev && !building && cron.start()
 
 export const handle: Handle = async ({ event, resolve }) => {
-	if (!validateBasicAuth()) return unauthenticated()
-
 	const path = event.url.pathname.slice(1)
 	if (path.length === ID_LENGTH && !path.includes('/'))
 		redirect(dev ? 307 : 308, `/afspraak/reageren/` + path)
