351b109bbcf53e0b926774d63e345b4a7d48efd7
qcoret <qcoret@gmail.com>
2026-02-20 12:14:52 +0100

    nit: simplify sitemap code

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -1,12 +1,16 @@
 import type { RequestHandler } from '@sveltejs/kit'
 
-import * as sitemap from 'super-sitemap'
+const PATHS = ['/', '/afspraak/aanmaken', '/privacy', '/voorwaarden']
 
 export const GET: RequestHandler = async () => {
-	return await sitemap.response({
-		origin: 'https://prikkert.nl',
-		additionalPaths: ['/afspraak/aanmaken'],
-		excludeRoutePatterns: ['/afspraak/*', '/afspraken/*'],
-		paramValues: { '/[page]': ['privacy', 'voorwaarden'] },
-	})
+	return new Response(
+		`<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
+${PATHS.map(
+				(path) => `	<url>
+		<loc>https://prikkert.nl${path}</loc>
+	</url>`,
+			).join('\n')}
+</urlset>`,
+		{ headers: { 'Content-Type': 'application/xml' } },
+	)
 }
