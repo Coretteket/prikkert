667497af04678dc043f353e8d41fed0676e01bea
qcoret <qcoret@gmail.com>
2026-03-03 12:14:46 +0100

    fix: workaround for https://github.com/sveltejs/kit/issues/7805

--- src/lib/shared/temporal.ts
+++ src/lib/shared/temporal.ts
@@ -1,3 +1,18 @@
-if (!('Temporal' in globalThis)) await import('temporal-polyfill/global')
+/**
+ * Because of this Webkit bug...
+ * https://bugs.webkit.org/show_bug.cgi?id=242740
+ *
+ * ...this SvelteKit issue exists:
+ * https://github.com/sveltejs/kit/issues/7805
+ *
+ * Therefore, dynamic import of polyfill only when needed is NOT possible.
+ * So we import the polyfill for all browsers, adding 20kb gzipped to the bundle.
+ *
+ * Thanks, Safari!
+ */
 
-export const Temporal = globalThis.Temporal
+// if (!('Temporal' in globalThis)) await import('temporal-polyfill/global')
+
+// export const Temporal = globalThis.Temporal
+
+export { Temporal } from 'temporal-polyfill'
