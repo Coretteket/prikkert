ad2847f834b523ffc47be3b6e3cfb3a70b71dcb0
qcoret <qcoret@gmail.com>
2026-03-14 00:24:47 +0100

    hotfix: responding broken in english

--- src/routes/afspraak/reageren/[eventId]/action.remote.ts
+++ src/routes/afspraak/reageren/[eventId]/action.remote.ts
@@ -64,8 +64,8 @@ function handleValidationErrors(issues: [v.BaseIssue<unknown>, ...v.BaseIssue<un
 }
 
 export const submitAvailability = form('unchecked', async (formData) => {
-	const { locals, params } = getRequestEvent()
-	const eventId = params.eventId
+	const { locals, url } = getRequestEvent()
+	const eventId = url.pathname.split('/').at(-1!)
 
 	if (!eventId) error(404, 'Afspraak niet gevonden')
 
